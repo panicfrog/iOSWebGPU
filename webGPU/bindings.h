9cde11a1eb05aca89e9e0807977e75da928b3628
yeyongping <burnedfrog@gmail.com>
2021-09-21 23:53:15 +0800

    configuration project

---
+++ webGPU/bindings.h
@@ -0,0 +1,14 @@
+//
+//  bindings.h
+//  webGPU
+//
+//  Created by Ye Yongping on 2021/9/21.
+//
+
+#ifndef bindings_h
+#define bindings_h
+
+void run_app(void);
+
+
+#endif /* bindings_h */
---
+++ webGPUTests/webGPUTests.swift
@@ -0,0 +1,33 @@
+//
+//  webGPUTests.swift
+//  webGPUTests
+//
+//  Created by Ye Yongping on 2021/9/21.
+//
+
+import XCTest
+@testable import webGPU
+
+class webGPUTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
